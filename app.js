a6e9857326239547274784fa31ccb3b54644ca7f
Shreyansh Goyal <110629672+Shreyanshgl25@users.noreply.github.com>
2023-06-23 13:00:36 +0530

    Error fix

--- app.js
+++ app.js
@@ -1,139 +1,174 @@
-const ps = require("prompt-sync"); // Dependency: prompt-sync for user input
+const ps = require("prompt-sync");
 const prompt = ps();
-const http = require("http"); // Built-in module for making HTTP requests
-const { json } = require("stream/consumers");
-const { response } = require("express");
-const { error } = require("console");
-var sessionID = "";
+const http = require("https");
 
-// Authentication credentials
-const data = JSON.stringify({
-  DomainName: 'GGV3',
-  UserName: 'Shreyansh',
-  Password: 'Shreyansh',
-});
+//Session to use on later purposes
+var sessionId = ""
 
-// Function to generate request options
-const option_Func = (meth, path, id) => {
+//Creating a function to pass on URL, Method Type and Session ID
+//https://www.famark.com/host/api.svc/api
+const  url = (path, sessionId) => {
   var options = {
-    host: "localhost",
-    port: 8092,
-    path: "/api.svc/api" + path,
-    method: meth,
+    host: "www.famark.com",
+    path: "/host/api.svc/api" + path,
+    method: 'POST',
     headers: {
-      SessionId: id,
+      "SessionId" : sessionId,
       "Content-Type": "application/json; charset=UTF-8",
-      Accept: "application/json; charset=UTF-8",
-    },
+      "Accept" : "application/json; charset=UTF-8"
+    }
   };
   return options;
 };
 
-let suffix = "/Credential/Connect";
-let metho = "POST";
+//Login request and getting reponse
+function login() {
+  
+  //Assigning Credentials
+  let DomainName = prompt("Enter Domain Name: ");
+  let UserName = prompt("Enter UserName: ");
+  let Password = prompt("Enter Password: ");
 
-// Request to authenticate and obtain session ID
-let httpreq = http.request(option_Func(metho, suffix, null), (response) => {
-  console.log("statusCode:", response.statusCode);
-  console.log("headers:", response.headers);
-
-  response.on("data", (d) => {
-    sessionID = JSON.parse(d);
-    console.log(typeof sessionID);
-    console.log(sessionID);
-  });
-});
-
-httpreq.on("error", (error) => {
-  console.log("An error", error);
-});
-httpreq.write(data);
-httpreq.end();
-
-// setTimeout(createRec, 500);
- setTimeout(retriveData,600);
-//setTimeout(() => DeleteRecord(), 600); // Trigger the DeleteRecord function after 600 milliseconds
-
-function createRec() {
-  // Code to create a new record
-  let rec_data = JSON.stringify({
-    FirstName: "Rahul",
-    LastName: "Sharma",
+  //Convert Credentials to JSON
+  const Creds = JSON.stringify({
+    DomainName,
+    UserName,
+    Password,
   });
 
-  let recpath = "/Business_Contact/CreateRecord";
-  let recmeth = "POST";
-  let option = option_Func(recmeth, recpath, sessionID);
-  console.log(JSON.stringify(option));
-  let httpreq2 = http.request(option, (response) => {
+  let data = url("/Credential/Connect", null)
+
+  //Posting Credentials in URL and generating response SessionID
+  let httpLoginRequest = http.request(data, (response) => {
     console.log("statusCode:", response.statusCode);
     console.log("headers:", response.headers);
-
+  
     response.on("data", (d) => {
-      process.stdout.write(d);
-      
+      sessionId = JSON.parse(d)
+      console.log('Session ID : ', sessionId)
     });
   });
-  httpreq2.on("error", (error) => {
-    console.log("An error", error.message);
+
+  httpLoginRequest.on("error", (error) => {
+    console.log("An error", error);
   });
-  httpreq2.write(rec_data);
-  httpreq2.end();
+
+  httpLoginRequest.write(Creds);
+
+  httpLoginRequest.end(); //End HTTP Request
 }
 
+//Create a New Record
+function createRecord() {
 
+  let ans = prompt("Do you want to create new Record? (Y or N) ");
 
+  if(ans == "Y" || ans == "y") {
+    let data = url("/Business_Contact/CreateRecord", sessionId);
+  
+    let FirstName = prompt("Enter First Name: ");
+    let LastName = prompt("Enter Last Name: ");
+    let Phone = prompt("Enter Phone:  ");
+    let Email = prompt("Enter Email: ")
 
-function retriveData() {
-  // Code to retrieve records
-   // let option = option_Func("GET","/Business_Contact/RetrieveMultipleRecords",sessionID);
-   const RETdata = JSON.stringify({
-    Columns:'FirstName,LastName,Business_ContactId',
-    OrderBy:'FirstName',
-  });
-  let options = {
-    host: "localhost",
-    method: 'POST',
-    port: 8092,
-    path: "/api.svc/api/Business_Contact/RetrieveMultipleRecords",
-    headers: {
-      'SessionId': sessionID,
-    },
-  };
-  let httpreq3 = http.request(options, (response) => {
-    console.log("statusCode:", response.statusCode);
-    console.log("headers:", response.headers);
-      response.on("data", (chunk) => {
-        console.log(JSON.parse(chunk));
+    let Contact =  JSON.stringify({
+      FirstName : FirstName,
+      LastName : LastName,
+      Phone : Phone,
+      Email : Email
+    });
+
+    let httpPostRequest = http.request(data, (response) => {
+
+      response.on("data", (d) => {
+        process.stdout.write(d);
       });
+
+    });
+
+    httpPostRequest.on("error", (error) => {
+      console.log("An error: ", error.message);
+    });
+
+    httpPostRequest.write(Contact);
+
+    httpPostRequest.end();
+
+    console.log("New Record Created");
+  
+  }
+}
+
+function retreiveRecord() {
+
+  //Listing desired data
+  let list = JSON.stringify({
+    Columns : 'FullName, Phone, Email, Business_ContactId',
+    OrderBy : 'FullName'
+  })
+  
+  let data = url("/Business_Contact/RetrieveMultipleRecords", sessionId)
+  
+  let httpGetRequest = http.request(data, (response) => {
+
+    response.on("data", (d) => {
+      console.log(JSON.parse(d));
+    });
+
+  }) 
+  .on("error", (error) => {
+
+    console.log("Error in Retreiving: ", error);
+
+  })
+
+  httpGetRequest.write(list);
+  httpGetRequest.end();
+
+};
+
+//f0d51aad-7182-4e33-ba54-3a35c782b00a
+
+function deleteRecord() {
+
+  let ans = prompt("Do you want to Delete Record?(Y or N)");
+
+  if(ans == "Y" || ans == "y") {
+
+    var recordId = prompt("Enter Record Id: ");
+
+    //Listing desired data
+    let list = JSON.stringify({
+      Business_ContactId : recordId
     })
+  
+    let data = url("/Business_Contact/DeleteRecord", sessionId)
+  
+    let httpGetRequest = http.request(data, (response) => {
+
+      response.on("data", (d) => {
+        console.log(JSON.parse(d));
+      });
+
+    }) 
     .on("error", (error) => {
-      console.log("Error in Retriving: ", error);
+
+      console.log("Error in Retreiving: ", error);
+
     })
-    httpreq3.write(RETdata)
-    httpreq3.end();
-}
 
+    httpGetRequest.write(list);
+    httpGetRequest.end();
 
+  }
 
-function DeleteRecord() {
-  // ID of the record to be deleted
-  let Del_data = JSON.stringify({
-    Business_ContactId: "48c6345a-85c8-4a37-8012-89752757361f",
-  });
+};
 
-  // Request options for deleting the record
-  let option = option_Func('POST', '/Business_Contact/DeleteRecord', sessionID);
+login();
 
-  // Send the delete request
-  const httpreq4 = http.request(option, (response) => {
-    console.log(response.statusCode)
-  });
+setTimeout(createRecord, 1000);
+setTimeout(deleteRecord, 1500);
+setTimeout(retreiveRecord, 1500);
+// setTimeout(deleteRecord, 1000);
 
-  httpreq4.on('error', (error) => {
-    console.log('error while Deleting :', error)
-  });
-
-  httpreq4.write(Del_data);
-  httpreq4.end();
-}
+// 7d86fc99-6c26-4067-9664-2c64165838d1
